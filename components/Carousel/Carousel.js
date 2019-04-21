fa250e145bd0474e5afba7bd276491ac98147374
JacobWellner83 <jcwellner83@gmail>
2019-04-21 15:40:31 -0500

    setEventHandlers, slideImage and sliderAuto

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -12,7 +12,39 @@ class Carousel {
         this.setEventHandlers();
         this.timer = this.sliderAuto();
     }
-}
+
+    setEventHandlers() {
+        this.buttons.forEach(button => {
+            button.element.addEventListener('click', (event) => {
+                this.slideImage(button.direction);
+            });
+        });
+ 
+        this.element.addEventListener('mouseenter', event => clearInterval(this.timer));
+        this.element.addEventListener('mouseleave', event => {
+            this.timer = this.sliderAuto()
+        });
+    }
+ 
+    slideImage(direction) {
+        let next;
+        if (direction === 'left') {
+            next = (this.current < this.images.length - 1) ? this.current + 1 : 0;
+        } else {
+            next = (this.current > 0) ? this.current - 1 : this.images.length - 1;
+        }
+ 
+        this.images[this.current].hide();
+        this.images[next].show();
+        this.current = next;
+    }
+ 
+    sliderAuto() {
+        return setInterval(() => {
+            this.slideImage('left');
+        }, 2200)
+    }
+ }
 
 class CarouselImage {
 
